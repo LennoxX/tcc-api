b2f75ea8d530e7d9934b309c70663cc6cd3f6c60
Lucas <lucas.ferreira@seati.ma.gov.br>
2019-07-08 10:05:46 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/br/uema/locacao/LocacaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.uema.locacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uema/locacao/LocacaoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.uema.locacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocacaoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
